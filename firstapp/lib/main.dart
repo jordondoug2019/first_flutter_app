d2f7fcc16a5511612b276099dfc9c914d014691e
Jordon Douglas <jordondouglas@Jordons-MacBook-Air.local>
2025-02-24 18:19:22 -0600

    created first Flutter function

---
+++ firstapp/lib/main.dart
@@ -0,0 +1,10 @@
+//import flutter package
+import 'package:flutter/material.dart';
+
+//Creating a custom Function
+//Main function is the main entry point for Flutter app
+void main () {
+  runApp(); //runApp function can only be ran within main function. Also comes from flutter
+}
+//Main() is executed automatically. runApp() tells Flutter which widget to draw onto the screen
+
