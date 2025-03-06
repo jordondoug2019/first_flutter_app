1d87f1bb069cba5b4b1cdcfe6a19164249b33f9a
Jordon Douglas <jordondouglas@Jordons-MacBook-Air.local>
2025-03-06 10:22:11 -0600

    Added text and color

--- firstapp/lib/main.dart
+++ firstapp/lib/main.dart
@@ -3,8 +3,33 @@ import 'package:flutter/material.dart';
 
 //Creating a custom Function
 //Main function is the main entry point for Flutter app
-void main () {
-  runApp(); //runApp function can only be ran within main function. Also comes from flutter
+void main() {
+  runApp( FirstApp()); //runApp function can only be ran within main function. Also comes from flutter
 }
-//Main() is executed automatically. runApp() tells Flutter which widget to draw onto the screen
 
+//Main() is executed automatically. runApp() tells Flutter which widget to draw onto the screen
+//widgets=objects=data structured memory
+//Generic types-flexible types that work together with other types
+
+class FirstApp extends StatelessWidget {
+  const FirstApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    //Implement Build
+    return MaterialApp(
+      //root
+      home: Scaffold(
+        //Scaffold is where you design
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [Colors.indigo, Colors.lightBlue],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight),
+          ),
+          child: Center(child: Text("Hello World!", style: TextStyle(color: Colors.white, fontSize: 23),)),
+        ),
+      ),
+    );
+  }
+}
